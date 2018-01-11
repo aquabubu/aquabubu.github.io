5a8307b4fb5dcee353640d59e3bfe5399499d37b
maidinh <maidinh@postech.ac.kr>
2018-01-11 17:44:37 +0900

    Update sites

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-3e52dd9e28f078e237b8.js","947818e789ba6640ab92a861db20343c"],["/commons-6473f4baabc271282128.js","dbf8d51f6d9c4604ea2bc206bcf7ef4d"],["/index.html","fca20c8be01442017199f0ba6019796f"],["/manifest.json","daa3017e200f854fcf7bbc803eb22b87"],["/offline-plugin-app-shell-fallback/index.html","16029ee7d7455ddebab737453e350394"]];
+var precacheConfig = [["/app-3e52dd9e28f078e237b8.js","947818e789ba6640ab92a861db20343c"],["/commons-6473f4baabc271282128.js","dbf8d51f6d9c4604ea2bc206bcf7ef4d"],["/index.html","c79211fc5918dd06495dbfc9486d9a78"],["/manifest.json","daa3017e200f854fcf7bbc803eb22b87"],["/offline-plugin-app-shell-fallback/index.html","471d1050b73515c802db74553eed74b7"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
